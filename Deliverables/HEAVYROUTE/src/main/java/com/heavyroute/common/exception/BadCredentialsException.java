403837b35c1c72ffe1cd2600963a1293b041d426
UgoManzoED <u.manzo2@studenti.unisa.it>
2026-01-08 17:14:06 +0100

    feat(common): aggiungte classi eccezione per conflitti utente e credenziali errate

---
+++ Deliverables/HEAVYROUTE/src/main/java/com/heavyroute/common/exception/BadCredentialsException.java
@@ -0,0 +1,7 @@
+package com.heavyroute.common.exception;
+
+public class BadCredentialsException extends RuntimeException {
+    public BadCredentialsException(String message) {
+        super(message);
+    }
+}
