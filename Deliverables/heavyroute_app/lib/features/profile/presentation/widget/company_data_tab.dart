873bbc2fe825e9b57ed05b0977d343a4bff8746e
umanfredini <u.manfredini@studenti.unisa.it>
2026-01-10 00:15:39 +0100

    feat(features-profile) Migliorata la sezione del profilo in accordo con i Mockup

---
+++ Deliverables/heavyroute_app/lib/features/profile/presentation/widget/company_data_tab.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CompanyDataTab extends StatelessWidget {
+  const CompanyDataTab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text("Qui andrà il Form Dati Aziendali"));
+  }
+}
